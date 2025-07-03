20d07845cffcccb4d9cb36c27d21939822db2a3c
AbdallahMostafaIbrahim <abdallahmostafaibrahim@gmail.com>
2025-07-03 16:11:09 +0300

    fibonacci test case

--- tests/ex-6.s
+++ tests/ex-6.s
@@ -2,32 +2,41 @@
 # File: ex-6.s
 # Tests: Fibonacci Series
 
+.equ STACK_TOP, 0xEFFE
+
 .text
 .org 0x000
     j  main
 .org 0x0020
 main:
-
-    li x6, 5
-    jal x1, Fib
+    li16 sp, STACK_TOP  # Initialize stack pointer
+    li a0, 8
+    call Fib
     # Final exit
     ecall 0x00A         # Exit system call
 
 Fib:
-    addi x2, -6
-    sw x1 , 0(x2)
-    sw x6, 2(x2)
-    addi x6, x6, -1
-    jal x1, Fib         # Fib(x-1)
+    li t0, 2
+    blt a0, t0, base_case
+    j continue
+    base_case:
+        ret
+    continue:
+    addi sp, -6
+    sw ra , 0(sp)
+    sw a0, 2(sp)
+    addi a0, -1
+    call Fib         # Fib(x-1)
 
-    sw x6, 4(x2)
-    lw x6, 2(x2)
-    addi x6, -2
+    sw a0, 4(sp)
+    lw a0, 2(sp)
+    addi a0, -2
 
-    jal x1, Fib         # Fib(x-1)
-    lw t0, 4(x2)
-    add x6, x0
-    lw x1, 0(x2)
-    addi x2, 6
+    call Fib        # Fib(x-1)
+    lw t0, 4(sp)
+    add  t0, a0
+    mv   a0, t0
+    lw ra, 0(sp)
+    addi sp, 6
 
-    jr x1       # return
+    ret
